5d1c73ad560904ee268e31b6739aad5344490020
Mokshavardhan <118769060+Mokshavardhan@users.noreply.github.com>
2025-07-25 16:53:26 +0530

    Update api.ts

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // 👇 Ensure this port number matches your running backend server (e.g., 3001)
-  baseURL: 'http://localhost:3002', 
+  baseURL: 'https://ai-component-generator1.onrender.com', 
 });
 
 export default api;
